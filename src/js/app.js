f624a719fac7c546310e92b930a3b0b6dfaf7f15
Maxim Terletskii <max.t95@bk.ru>
2024-12-22 17:14:57 +0300

    HW Corrections

--- src/js/app.js
+++ src/js/app.js
@@ -25,69 +25,54 @@ class Character {
         this.type = type;
         this.health = 100;
         this.level = 1;
-
-        switch (type) {
-            case 'Bowman':
-                this.attack = 25;
-                this.defence = 25;
-                break;
-            case 'Swordsman':
-                this.attack = 40;
-                this.defence = 10;
-                break;
-            case 'Magician':
-                this.attack = 10;
-                this.defence = 40;
-                break;
-            case 'Undead':
-                this.attack = 25;
-                this.defence = 25;
-                break;
-            case 'Zombie':
-                this.attack = 40;
-                this.defence = 10;
-                break;
-            case 'Daemon':
-                this.attack = 10;
-                this.defence = 40;
-                break;
-        }
     }
 }
 
 class Bowerman extends Character {
     constructor(name) {
         super(name, 'Bowman');
+        this.attack = 25;
+        this.defence = 25;
     }
 }
 
 class Swordsman extends Character {
     constructor(name) {
         super(name, 'Swordsman');
+        this.attack = 40;
+        this.defence = 10;
     }
 }
 
 class Magician extends Character {
     constructor(name) {
         super(name, 'Magician');
+        this.attack = 10;
+        this.defence = 40;
     }
 }
 
 class Daemon extends Character {
     constructor(name) {
         super(name, 'Daemon');
+        this.attack = 10;
+        this.defence = 40;
     }
 }
 
 class Undead extends Character {
     constructor(name) {
         super(name, 'Undead');
+        this.attack = 25;
+        this.defence = 25;
     }
 }
 
 class Zombie extends Character {
     constructor(name) {
         super(name, 'Zombie');
+        this.attack = 40;
+        this.defence = 10;
     }
 }
 
